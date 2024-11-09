c48c5fa39ae367d56723005ebae732fa71cbd93b
Jean-Marc Strauven <jms@grazulex.be>
2024-11-09 19:54:44 +0100

    add global inventory

--- scripts/items/collectable.gd
+++ scripts/items/collectable.gd
@@ -22,6 +22,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if is_nearby and Input.is_action_just_pressed(action_key):
 		print("get: " + collectable_name)
+		InventoryManager.add_collectable(collectable_name)
 		queue_free()
 
 func _on_collect() -> void:
