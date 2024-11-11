cfc17f4f0460c35ff5f82631e54d7de83213a898
Jean-Marc Strauven <jms@grazulex.be>
2024-11-11 08:12:50 +0100

    add egg

--- scripts/ui/inventory_ui.gd
+++ scripts/ui/inventory_ui.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 @onready var log_label: Label = $MarginContainer/VBoxContainer/Logs/LogLabel
 @onready var stone_label: Label = $MarginContainer/VBoxContainer/Stone/StoneLabel
 @onready var milk_label: Label = $MarginContainer/VBoxContainer/Milk/MilkLabel
+@onready var egg_label: Label = $MarginContainer/VBoxContainer/Egg/EggLabel
 
 
 func _ready() -> void:
@@ -20,3 +21,6 @@ func on_inventory_changed() -> void:
 		
 	if inventory.has("Milk"):
 		milk_label.text = str(inventory["Milk"])
+		
+	if inventory.has("Egg"):
+		egg_label.text = str(inventory["Egg"])
